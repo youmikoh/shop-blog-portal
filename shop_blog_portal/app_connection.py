7e6814588e45394a850620d7144c5acc76dc2bc2
Youmi <29780339+youmikoh@users.noreply.github.com>
2021-12-08 09:06:44 -0500

    Add import command

--- shop_blog_portal/app_connection.py
+++ shop_blog_portal/app_connection.py
@@ -2,6 +2,7 @@
 
 import json
 import click
+from typing import List, Dict
 
 import shopify
 
@@ -16,21 +17,63 @@ class PrivateApp:
     def connect(self) -> None:
         shopify.ShopifyResource.set_site(self.shop_url)  # type: ignore
 
-    def get_blogs(self):
-        return shopify.Blog.find()
+    def get_posted_blogs(self) -> Dict[str, shopify.Blog]:
+        return {b.attributes["handle"]: b for b in shopify.Blog.find()}
 
     def export_blogs(self, output_file: str) -> None:
-        blogs = self.get_blogs()
+        posted_blogs = self.get_posted_blogs()
 
-        for b in blogs:
-            b.attributes["articles"] = b.articles()
+        for handle, blog in posted_blogs.items():
+            blog.attributes["articles"] = blog.articles()
+            posted_blogs[handle] = blog.to_dict()
 
-        blogs_export = [b.to_dict() for b in blogs]
         with open(output_file, "w") as f:
-            json.dump(blogs_export, f, indent=4)
+            json.dump(posted_blogs, f, indent=4)
 
-    def import_blogs(self, file: str) -> None:
-        pass
+    def post_article(self, blog_id: str, article: shopify.Article) -> None:
+        a = {
+            "blog_id": blog_id,
+            "title": article["title"],
+            "handle": article["handle"],
+            "author": article.get("author"),
+            "tags": article.get("tags"),
+            "body_html": article.get("body_html"),
+            "image": article.get("image"),
+        }
+        shopify.Article.create(a)
+        print("Added: ", article.get("handle"))
+
+    def import_posted_blog(self, posted_blog: shopify.Blog, exported_articles: List[shopify.Article]) -> None:
+        posted_articles = set(a.to_dict().get("handle") for a in posted_blog.articles())
+        for article in exported_articles:
+            if article["handle"] not in posted_articles:
+                self.post_article(posted_blog.id, article)
+
+    def import_new_blog(self, blog: shopify.Blog, exported_articles: List[shopify.Article]) -> None:
+        b = {
+            "title": blog["title"],
+            "handle": blog["handle"],
+        }
+        new_blog = shopify.Blog.create(b)
+        for article in exported_articles:
+            self.post_article(new_blog.id, article)
+
+    def import_blogs(self, input_file: str) -> None:
+        posted_blogs = self.get_posted_blogs()
+
+        with open(input_file, "r") as f:
+            exported_blogs = json.load(f)
+
+        for handle, blog in exported_blogs.items():
+            exported_articles = blog["articles"]
+            posted_blog = posted_blogs.get(handle)
+
+            if posted_blog:
+                # print(handle, "blog exists: importing only new articles")
+                self.import_posted_blog(posted_blog, exported_articles)
+            else:
+                # print(handle, "blog does not exist: importing entire blog, including all articles")
+                self.import_new_blog(blog, exported_articles)
 
 
 @click.group()
@@ -38,7 +81,7 @@ def cli():
     pass
 
 
-@cli.command()
+@cli.command(name="export")
 @click.option(
     "--store-name",
     prompt="Enter store name",
@@ -61,7 +104,7 @@ def cli():
     default="blog_export.json",
     help="Output file path.",
 )
-def export(store_name: str, api_key: str, password: str, output_file: str):
+def export_blogs(store_name: str, api_key: str, password: str, output_file: str):
     app = PrivateApp(
         shop=store_name,
         api_key=api_key,
@@ -70,6 +113,40 @@ def export(store_name: str, api_key: str, password: str, output_file: str):
 
     app.connect()
     app.export_blogs(output_file)
+
+
+@cli.command(name="import")
+@click.option(
+    "--store-name",
+    prompt="Enter store name",
+    help="Store to export from.",
+)
+@click.option(
+    "--api-key",
+    prompt="Enter private app api_key",
+    help="Private app API key.",
+)
+@click.option(
+    "--password",
+    prompt="Enter private app password",
+    hide_input=True,
+    help="Private app password.",
+)
+@click.option(
+    "--input-file",
+    type=click.Path(dir_okay=False, writable=False, readable=True),
+    default="blog_export.json",
+    help="Input file path.",
+)
+def import_blogs(store_name: str, api_key: str, password: str, input_file: str):
+    app = PrivateApp(
+        shop=store_name,
+        api_key=api_key,
+        password=password,
+    )
+
+    app.connect()
+    app.import_blogs(input_file)
 
 
 if __name__ == "__main__":
